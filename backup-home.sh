56d23f29a003f79e8de14f097a199cba2f9de7df
abhinav-deep <abhinavdeep.dev@gmail.com>
2022-01-26 01:20:29 +0530

    version-02

--- backup-home.sh
+++ backup-home.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
-#This is a bash script to backup user's home directory to /tep/.
+#This is a bash script to backup user's home directory to /tmp/.
 
 user=$(whoami)
 input=/home/$user
 output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
-tar -czf $output $input
+echo "Creating backup..."
+tar -czf $output $input 2> /dev/null
 echo "Backup of $input completed! Details about the output backup file: "
 ls -l $output
